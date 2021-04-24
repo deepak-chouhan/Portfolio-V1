834031d0e75f8cde388c6e18fd26ed0bc14f4f07
deepak <deepakceo0@gmail.com>
2021-04-24 11:46:47 +0530

    added sticky navbar functinality

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,11 +15,26 @@ $(document).ready(function () {
 
     var navblur = document.getElementById("navblur");
 
+    var desktopNav = document.getElementById("js-nav")
+
+
     mobilenavbar.style.width = "0px";
     navblur.style.opacity = "0"
     navblur.style.pointerEvents = "none"
 
 
+    // Sticky navbar
+    $(".js-section-aboutme").waypoint(function (direction) {
+
+        if (direction == "down") {
+            $("nav").css('box-shadow', '0px 4px 20px rgba(10, 25, 47, 0.1)');
+        } else {
+            $("nav").css('box-shadow', 'none');
+        }
+
+    }, {
+        offset: "80px;"
+    })
 
     $(mobilenavicon).click(function () {
 
@@ -28,15 +43,24 @@ $(document).ready(function () {
             menu_icon.style.fill = neon;
             navblur.style.opacity = "1"
             navblur.style.pointerEvents = "all"
-
-
         } else {
             mobilenavbar.style.width = "0px";
             menu_icon.style.fill = navyl;
             navblur.style.opacity = "0"
             navblur.style.pointerEvents = "none"
         }
-
     });
+
+    $(mobilenavicon).hover(function () {
+        // over
+        menu_icon.style.fill = neon;
+
+    }, function () {
+        // out
+        if (mobilenavbar.style.width !== "250px") {
+            menu_icon.style.fill = navyl;
+        }
+    });
+
 
 });

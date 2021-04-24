c6133b21011e6f7e9fcab94adf53f9883989d24d
deepak <deepakceo0@gmail.com>
2021-04-24 08:21:29 +0530

    added blur when nav open

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,9 +10,15 @@ $(document).ready(function () {
     var menu_btn = document.getElementById("mobilenavicon");
 
     var mobilenavbar = document.getElementById("mobilenavbar");
-    var menu_icon = document.getElementById("menu_icon")
+
+    var menu_icon = document.getElementById("menu_icon");
+
+    var navblur = document.getElementById("navblur");
 
     mobilenavbar.style.width = "0px";
+    navblur.style.opacity = "0"
+    navblur.style.pointerEvents = "none"
+
 
 
     $(mobilenavicon).click(function () {
@@ -20,10 +26,15 @@ $(document).ready(function () {
         if (mobilenavbar.style.width === "0px") {
             mobilenavbar.style.width = "250px";
             menu_icon.style.fill = neon;
+            navblur.style.opacity = "1"
+            navblur.style.pointerEvents = "all"
+
 
         } else {
             mobilenavbar.style.width = "0px";
             menu_icon.style.fill = navyl;
+            navblur.style.opacity = "0"
+            navblur.style.pointerEvents = "none"
         }
 
     });

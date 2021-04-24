7a1f776a1463545acb1245b492392b3f681138cc
deepak <deepakceo0@gmail.com>
2021-04-24 20:47:12 +0530

    added navigating to sections when clicking on links in nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -35,8 +35,7 @@ $(document).ready(function () {
         offset: "200px;"
     })
 
-    $(mobilenavicon).click(function () {
-
+    function navfunctioning() {
         if (mobilenavbar.style.width === "0px") {
             mobilenavbar.style.width = "250px";
             menu_icon.style.fill = neon;
@@ -48,6 +47,18 @@ $(document).ready(function () {
             navblur.style.opacity = "0"
             navblur.style.pointerEvents = "none"
         }
+    }
+
+    $(mobilenavicon).click(function () {
+        navfunctioning();
+    });
+
+    $(".work-link").click(function (e) {
+        navfunctioning();
+    });
+
+    $(".contact-link").click(function (e) {
+        navfunctioning();
     });
 
     $(mobilenavicon).hover(function () {
@@ -90,5 +101,46 @@ $(document).ready(function () {
     }, {
         offset: "90%"
     });
+
+    // Nav scroll
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+
+
 
 });

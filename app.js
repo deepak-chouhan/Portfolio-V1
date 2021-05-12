5e0a4710e11f51db249b52b588aaad00217c6cfc
deepak <deepakceo0@gmail.com>
2021-05-12 16:30:05 +0530

    added procfile

--- app.js
+++ app.js
@@ -213,6 +213,10 @@ app.post("/uploadproject", function (req, res) {
     res.redirect("/uploadproject")
 })
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000")
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+    console.log("Server started successfully")
 })
